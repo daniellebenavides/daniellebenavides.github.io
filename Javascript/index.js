2c13b3d2b026a3af7c0b6ca0b38fcbc23b6fea90
daniellebenavides <117321380+daniellebenavides@users.noreply.github.com>
2022-12-04 00:26:08 -0600

    Add files via upload

---
+++ Javascript/index.js
@@ -0,0 +1,22 @@
+$(document).ready(()=> {
+
+
+const projects=document.querySelectorAll(".galleryArt");
+
+projects.forEach(project=> {
+    project.addEventListener("click", function() {
+        document.querySelector("body").style.overflow="hidden"
+    })
+    
+});
+const overlay =document.getElementsByClassName("lightboxOverlay")[0];
+
+document.getElementsByClassName("lightboxOverlay")[0].addEventListener("click", function() {
+
+    document.querySelector("body").style.overflow="unset"
+})
+document.getElementsByClassName("lb-close")[0].addEventListener("click", function() {
+    
+    document.querySelector("body").style.overflow="unset"
+})
+})
